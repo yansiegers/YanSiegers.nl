fb38ea080c3dbbff57d8cc872516133b3076b8bb
Yan Siegers <email@yansiegers.nl>
2020-10-18 18:03:55 +0200

    Add 404 blade

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <title>Yan Siegers Portfolio</title>
+
+      <!-- Fonts -->
+      {{-- <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet"> --}}
+
+      <!-- Styles -->
+      <link rel="stylesheet" href="/build/tailwind.css">
+  </head>
+  <body>
+    <div class="container">
+      {{-- <img src="404.png" alt="404"> --}}
+      <img class="w-64" src="https://cybersport.pl/wp-content/uploads/2019/04/error404_logo.png" alt="404">
+    </div>
+  </body>
+</html>
