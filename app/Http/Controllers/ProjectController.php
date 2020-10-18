35eb8c5f81a4263abd917fffdba5518f678af5f1
Yan Siegers <email@yansiegers.nl>
2020-10-18 15:32:06 +0200

    Add routes for projects

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('projects.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('projects.show', ['project' => Project::findOrFail($id)]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/get-in-contact', function () {
+    return view('contact');
+});
+
+
+
+
+
+use App\Http\Controllers\ProjectController;
+// Route::get('/projects', [ProjectController::class, 'index']);
+// Route::get('project/{id}', [ProjectController::class, 'show']);
+Route::resource('projects', ProjectController::class)->only([
+    'index', 'show'
+]);
+
+
+
+
+// Route::get('/blog', function () {
+//     return view('blog');
+// });
+
+// use App\Http\Controllers\BlogPostsController;
+// Route::get('/blog/posts', [BlogPostsController::class, 'index']);
+
+// Route::get('/', function () {
+//     return view('home');
+// });
