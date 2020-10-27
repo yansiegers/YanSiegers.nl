d1a8b0b0b5ba6111ce02696a5d9cf70a169feca1
Yan Siegers <email@yansiegers.nl>
2020-10-27 18:38:59 +0100

    Install stylelint

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 {{-- <link rel="shortcut icon" href="icon-black.png" type="image/png"> --}}
-<link rel="shortcut icon" href="icon-white.png" type="image/png">
+<link rel="shortcut icon" href="/assets/icons/icon-white.png" type="image/png">
 
 <meta name="description" content="">
 <meta name="keywords" content="">
@@ -10,6 +10,7 @@
 <title>Yan Siegers {{ isset($title) ? Str::title(' – '.$title) : '' }}</title>
 
 <!-- Styles -->
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="/build/tailwind.css">
 
 <style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,30 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        // serif: ['"Source Serif Pro"', 'serif'],
+        sans: ['Roboto', "Helvetica Neue", 'Arial', 'sans-serif'],
+      },
+      // colors: {
+      //   fdblue: '#00a4e8',
+      //   fdorange: '#ff6633',
+      //   fdgreen: '#7cb44f',
+      //   fdwhite: '#ffffff',
+      //   fdgray: {
+      //     default: '#ededed',
+      //     darker: '#c4c4c4'
+      //   },
+      //   fdblack: '#000000'
+      // },
+      // boxShadow: {
+      //   lgr: '0 -10px 15px -3px rgba(0, 0, 0, .1), 0 -4px 6px -2px rgba(0, 0, 0, .05)'
+      // }
+    },
   },
-  variants: {},
-  plugins: []
+  variants: {
+    // backgroundColor: ['responsive', 'hover', 'focus', 'even'],
+    // borderWidth: ['responsive', 'hover', 'focus'],
+  },
+  plugins: [],
 }
